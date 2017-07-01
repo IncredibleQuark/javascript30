d2e4c18dc3a4157167cc7642a181d172746c4533
IncredibleQuark <lkupinski@gmail.com>
2017-07-01 22:50:48 +0200

    check checkboxes with shift

---
+++ 10_shiftCheckboxes/app.js
@@ -0,0 +1,29 @@
+/**
+ * Created by lkupinski on 01.07.2017.
+ */
+
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+
+    let inBetween = false;
+
+    if(e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        })
+    }
+
+    lastChecked = this;
+
+}
+
+checkboxes.forEach(checkbox =>checkbox.addEventListener('click', handleCheck));
